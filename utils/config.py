f2337084d714fb1d7ce513559b7a9ffa45d32eaf
Aleksandr Piskarev <piskarev_alexandr@mail.ru>
2026-04-25 15:41:58 +0300

    add config in utilis

--- utils/config.py
+++ utils/config.py
@@ -4,3 +4,8 @@ import os
 load_dotenv()
 
 BASE_URL = os.getenv("BASE_URL", "https://www.saucedemo.com")
+STANDARD_USER = os.getenv("STANDARD_USER", "standard_user")
+LOCKED_USER = os.getenv("LOCKED_USER", "locked_out_user")
+PASSWORD = os.getenv("PASSWORD", "secret_sauce")
+BROWSER = os.getenv("BROWSER", "chromium")
+HEADLESS = os.getenv("HEADLESS", "true").lower() == "true"
